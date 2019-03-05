43a438940f22cc79ab67730db92d03b7d58e2013
orjwan98 <orjwan98@hotmail.com>
2019-03-05 16:30:16 +0200

    use Material UI to make post component

--- client/src/Components/Post/index.js
+++ client/src/Components/Post/index.js
@@ -1,13 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import ExpansionPanel from "@material-ui/core/ExpansionPanel";
 import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
 import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import down from "../../assets/down.png";
+import up from "../../assets/up.png";
+import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 
 const styles = theme => ({
   root: {
+    margin: "auto",
     width: "50%"
   },
   heading: {
@@ -15,24 +19,39 @@ const styles = theme => ({
     fontWeight: "bold",
     color: "white"
   },
+
+  expand: {
+    fontSize: theme.typography.pxToRem(12),
+    color: "white"
+  },
+
   title: {
-    backgroundColor: "orange"
+    backgroundColor: "blue"
+  },
+  imgcontainer: {
+    display: "inline-block"
+  },
+  button: {
+    margin: "10px"
   }
 });
 
-const Post = ({ classes }) => {
+const Post = ({ classes, key, title, post, upvotes, downvotes }) => {
   return (
     <div className={classes.root}>
       <ExpansionPanel>
         <ExpansionPanelSummary className={classes.title}>
-          <Typography className={classes.heading}>Expansion Panel 1</Typography>
+          <Typography className={classes.heading}>{title}</Typography>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
+          <Typography>{post}</Typography>
         </ExpansionPanelDetails>
+        <Button color="primary" className={classes.button}>
+          Upvote {upvotes}
+        </Button>
+        <Button color="primary" className={classes.button}>
+          Downvote {downvotes}
+        </Button>
       </ExpansionPanel>
     </div>
   );
