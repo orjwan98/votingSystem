dd235d88d9c4b960f80fda9c0e5dc8ee5b11e3ef
orjwan98 <orjwan98@hotmail.com>
2019-03-06 09:18:34 +0200

    addeed registerUser/post route and handler

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const getData = require("./getData.js");
+const registerUser = require("./registerUser.js");
 
 router.get("/getData", getData.get);
+router.post("/registerUser", registerUser.post);
 
 module.exports = router;
