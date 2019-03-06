c79b0079bc45d9b2d61ec919c647bf92d9e3fb41
orjwan98 <orjwan98@hotmail.com>
2019-03-06 09:15:35 +0200

    render name in App

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import PostContainer from "./Components/PostContainer";
 import axios from "axios";
-
+import Name from "./Components/Name";
 class App extends Component {
   state = {
     data: null
@@ -16,6 +16,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Name />
         <PostContainer data={this.state.data} />
       </div>
     );
