18617d9733f352c22c365cac81e27e0506894300
orjwan98 <orjwan98@hotmail.com>
2019-03-06 09:16:52 +0200

    rearranged code to work

--- src/app.js
+++ src/app.js
@@ -5,10 +5,10 @@ const routes = require("./routes/index.js");
 var bodyParser = require("body-parser");
 
 require("env2")("config.env");
-
-app.use(routes);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "..", "client", "public")));
+
+app.use(routes);
 
 module.exports = app;
